f3a85291c62fe7784c30b7b53e7e823db1ff006b
rebeccafinne <rebecca@finne.se>
2018-09-23 15:59:52 +0200

    Started with hovering effect

--- config.js
+++ config.js
@@ -1,24 +1,36 @@
+
+
 function genGrid(){
   let maxAmount = 16;
   let grid = document.getElementById("container");
 
   for(var i = 0; i < maxAmount; i++){
-    var row = document.createElement("div");
-    row.className = "row";
+
+    /*var row = document.createElement("div");
+    row.className = "row";*/
 
     for(var x = 0; x < maxAmount; x++){
       var cell = document.createElement("div");
       cell.className = "gridsquare";
-      cell.style.backgroundColor = "blue";
+      cell.id = "gridsquare";
       cell.innerText = "Hello";
-      row.appendChild(cell);
+      //row.appendChild(cell);
+      grid.appendChild(cell);
     }
-    grid.appendChild(row);
-  }
+
+    }
+
 
 };
 
+/*function hover(){
+  item.setAttribute("style", "background-color:black;")
+}*/
 
 $(document).ready(function() {
     genGrid();
+
+  /*  $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+      });*/
   });
