3871dd1acffb69447cd2acf009cfd3d3cb491537
rebeccafinne <rebecca@finne.se>
2018-10-04 16:24:55 +0200

    Done with project need to be designed now

--- config.js
+++ config.js
@@ -1,36 +1,42 @@
+let maxAmount = 16;
 
-
-function genGrid(){
-  let maxAmount = 16;
+function genGrid(maxAmount){
   let grid = document.getElementById("container");
-
   for(var i = 0; i < maxAmount; i++){
-
-    /*var row = document.createElement("div");
-    row.className = "row";*/
-
     for(var x = 0; x < maxAmount; x++){
       var cell = document.createElement("div");
       cell.className = "gridsquare";
       cell.id = "gridsquare";
-      cell.innerText = "Hello";
+      cell.style.width = 100 / maxAmount + "%";
+			cell.style.height = 100 / maxAmount + "%";
       //row.appendChild(cell);
+      cell.addEventListener("mouseover", myScript);
+
       grid.appendChild(cell);
     }
-
-    }
-
-
+  }
 };
 
-/*function hover(){
-  item.setAttribute("style", "background-color:black;")
-}*/
+function resetScreen(){
+  let number = window.prompt("How big do you want the grid to be? \n Max 64!","16");
+  if(number > 64 ){
+    resetScreen();
+
+  }else{
+    removeGrid();
+    maxAmount = number;
+    genGrid(maxAmount);
+  }
+};
+
+function removeGrid(){
+  $('.gridsquare').remove();
+};
+
+function myScript(){
+  $(this).css("background-color", "black");
+};
 
 $(document).ready(function() {
-    genGrid();
-
-  /*  $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-      });*/
-  });
+    genGrid(maxAmount);
+});
